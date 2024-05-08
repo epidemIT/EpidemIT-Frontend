ab3e8544ec8c473d1b36a14b3d2fd1052c5a25c5
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2024-05-09 01:18:31 +0700

    feat:project modal 2 (unresponsive)

--- src/app/dashboard/components/ProjectModal.tsx
+++ src/app/dashboard/components/ProjectModal.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+import Image from "next/image";
 
 import {
   Dialog,
@@ -12,21 +13,41 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+  SelectLabel,
+} from "@/components/ui/select";
+
+import { Textarea } from "@/components/ui/textarea";
+
 const ProjectModal = () => {
-  const [modal, setModal] = useState(false);
+  const [modal1, setModal1] = useState(false);
+  const [modal2, setModal2] = useState(false);
+
   return (
     <div>
       <Dialog>
         <DialogTrigger>
           <div
             className="py-4 px-10 bg-[#2FBFA6] align-center justify-center rounded-[56px] text-white shadow-lg"
-            onClick={() => setModal(true)}
+            onClick={() => setModal1(true)}
           >
             Book Session
           </div>
         </DialogTrigger>
-        <DialogContent className="max-w-[45%]">
-          <div className="">
+        <DialogContent
+          className="max-w-[45%]"
+          onCloseAutoFocus={() => {
+            setModal1(false);
+            setModal2(false);
+          }}
+        >
+          <div className={`${modal1 ? `block` : `hidden`}`}>
             <p className="text-[#8C8C8C] text-md font-semibold ">STEP 1 OF 2</p>
             <h2 className="text-2xl text-[#031E31] mt-6 font-semibold ">
               Available Sessions
@@ -36,25 +57,25 @@ const ProjectModal = () => {
               <span className="font-bold text-lg">Asep Berlian</span>
             </h2>
             <div className="flex gap-5 mt-4 items-center justify-center">
-              <div className="flex flex-col justify-between items-center px-6 py-5 border-[1px] border-[#8C8C8C] rounded-[10px]">
+              <div className="flex flex-col justify-between items-center px-8 py-5 border-[1px] border-[#8C8C8C] rounded-[10px]">
                 <p className="text-md text-[#8C8C8C] font-medium">Wed</p>
                 <h1 className="text-black font-bold text-[40px]">08</h1>
                 <h1 className="text-black font-bold text-[20px]">May</h1>
                 <p className="text-md text-[#26B43C]">10 slots</p>
               </div>
-              <div className="flex flex-col justify-between items-center px-6 py-5 border-[2px] border-black rounded-[10px]">
+              <div className="flex flex-col justify-between items-center px-8 py-5 border-[2px] border-black rounded-[10px]">
                 <p className="text-md text-[#8C8C8C] font-medium">Thu</p>
                 <h1 className="text-black font-bold text-[40px]">09</h1>
                 <h1 className="text-black font-bold text-[20px]">May</h1>
                 <p className="text-md text-[#26B43C]">10 slots</p>
               </div>
-              <div className="flex flex-col justify-between items-center px-6 py-5 border-[1px] border-[#8C8C8C] rounded-[10px]">
+              <div className="flex flex-col justify-between items-center px-8 py-5 border-[1px] border-[#8C8C8C] rounded-[10px]">
                 <p className="text-md text-[#8C8C8C] font-medium">Fri</p>
                 <h1 className="text-black font-bold text-[40px]">10</h1>
                 <h1 className="text-black font-bold text-[20px]">May</h1>
                 <p className="text-md text-[#26B43C]">10 slots</p>
               </div>
-              <div className="flex flex-col justify-between items-center px-6 py-5 border-[1px] border-[#8C8C8C] rounded-[10px]">
+              <div className="flex flex-col justify-between items-center px-8 py-5 border-[1px] border-[#8C8C8C] rounded-[10px]">
                 <p className="text-md text-[#8C8C8C] font-medium">Sat</p>
                 <h1 className="text-black font-bold text-[40px]">11</h1>
                 <h1 className="text-black font-bold text-[20px]">May</h1>
@@ -89,7 +110,88 @@ const ProjectModal = () => {
               </p>
             </div>
             <div className="flex justify-center items-center">
-              <div className="flex justify-center items-center mt-5 text-white rounded-full w-[80%] py-3 bg-[#2FBFA6]">
+              <div
+                className="flex justify-center items-center mt-5 text-white rounded-full w-[80%] py-3 bg-[#2FBFA6]"
+                onClick={() => {
+                  setModal1(false);
+                  setModal2(true);
+                }}
+              >
+                Continue
+              </div>
+            </div>
+          </div>
+
+          <div className={`${modal2 ? `block` : `hidden`}`}>
+            <p className="text-[#8C8C8C] text-md font-semibold ">STEP 2 OF 2</p>
+            <h2 className="text-2xl text-[#031E31] mt-6 font-semibold ">
+              Confirm Booking
+            </h2>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-4 mt-4">
+                <Image
+                  src={"/gibs.jpeg"}
+                  alt="Profile"
+                  width={150}
+                  height={150}
+                  className="h-[120px] w-[120px] rounded-full"
+                />
+                <div>
+                  <h2 className="text-xl font-semibold ">Asep Berlian</h2>
+                  <p className="font-medium text-[#2A638B] text-lg">
+                    Software Engineer at Mostrans
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex justify-between items-center gap-10 px-4 py-4 border-[1px] h-fit">
+                <div>
+                  <p className="text-xl font-bold">09 May 2024</p>
+                  <p className="text-lg font-medium text-[#8C8C8C]">12:30</p>
+                </div>
+                <p className="text-[#2FBFA6]">Change</p>
+              </div>
+            </div>
+            <hr className=" mt-6 h-[1px] w-[90%] mx-auto bg-[#8C8C8C]" />
+            <h3 className="text-2xl font-medium mt-5">Booking Details</h3>
+
+            <div className="flex gap-10 mt-6">
+              <div>
+                <p className="text-md font-semibold mb-4">Select Main Topic</p>
+                <Select>
+                  <SelectTrigger className="w-[300px]">
+                    <SelectValue placeholder="Please Select" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Main TOpic</SelectLabel>
+                      <SelectItem value="data">Data</SelectItem>
+                      <SelectItem value="code">COde</SelectItem>
+                      <SelectItem value="oop">OOP</SelectItem>
+                      <SelectItem value="design">Design</SelectItem>
+                      <SelectItem value="web">Web</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div>
+                <p className="text-md font-semibold mb-4">
+                  Add your questions for this session
+                </p>
+                <Textarea
+                  className="w-[360px]"
+                  placeholder="Type your message here."
+                />
+              </div>
+            </div>
+            <div className="flex justify-center items-center">
+              <div
+                className="flex justify-center items-center mt-5 text-white rounded-full w-[80%] py-3 bg-[#2FBFA6]"
+                onClick={() => {
+                  setModal1(false);
+                  setModal2(true);
+                }}
+              >
                 Continue
               </div>
             </div>
