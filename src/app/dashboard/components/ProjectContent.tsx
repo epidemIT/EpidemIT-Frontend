89908def6b158370422c33ec985e7a015a07eb2a
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-05-08 23:20:58 +0700

    feat: datoCMS integration

--- src/app/dashboard/components/ProjectContent.tsx
+++ src/app/dashboard/components/ProjectContent.tsx
@@ -4,12 +4,23 @@ import Navbar from "@/components/Navbar";
 import ProgressBar from "@ramonak/react-progress-bar";
 import Image from "next/image";
 import React from "react";
+import {
+  Image as ResponsiveImage,
+  StructuredText,
+  VideoPlayer,
+} from "react-datocms";
 
 export interface CreateSoundRequest {
   text: string;
 }
 
-export default function ProjectContent() {
+
+export default function ProjectContent({
+  allMaterials,
+  projectName,
+  partnerName,
+  progress,
+}: any) {
   /*
   const [audioUrl, setAudioUrl] = useState<string | null>(null);
   const handleGetAudio = async (request: CreateSoundRequest) => {
@@ -41,7 +52,9 @@ export default function ProjectContent() {
   };
   Api tts */
 
-  const progress = 52;
+  const [currentMateriKe, setCurrentMateriKe] = useState(1);
+  const [currentMateri, setCurrentMateri] = useState(allMaterials[0]);
+
   return (
     <div className="py-8 px-[11%]">
       <div className="flex justify-center">
@@ -50,35 +63,37 @@ export default function ProjectContent() {
       <div className="w-full flex gap-16">
         <div className="w-2/5 flex flex-col gap-12 mt-12">
           <div>
-            <h1 className="text-2xl font-bold">Project Name</h1>
-            <p className="text-gray-500 text-lg">Project Company</p>
+            <h1 className="text-2xl font-bold">{projectName}</h1>
+            <p className="text-gray-500 text-lg">{partnerName}</p>
           </div>
 
           <div className="bg-[#FBFBFB] rounded-lg">
-            <div className="p-6 cursor-pointer text-lg border-b-2">
-              <p>Minggu 1</p>
-            </div>
-            <div className="p-6 cursor-pointer text-lg border-b-2">
-              <p>Minggu 2</p>
-            </div>
-            <div className="p-6 cursor-pointer text-lg border-b-2">
-              <p>Minggu 3</p>
-            </div>
-            <div className="p-6 cursor-pointer text-lg">
-              <p>Minggu 4</p>
-            </div>
+            {allMaterials.map((material: any, index: number) => (
+              <div
+                key={index}
+                className={`p-6 cursor-pointer text-lg border-b-2 ${
+                  currentMateriKe === index + 1 ? "bg-primary text-white" : ""
+                }`}
+                onClick={() => {
+                  setCurrentMateriKe(index + 1);
+                  setCurrentMateri(material);
+                }}
+              >
+                <p>Materi {index + 1}</p>
+              </div>
+            ))}
           </div>
 
           <div className="flex flex-col gap-4">
             <div className="text-2xl flex justify-between">
               <p className="font-semibold">Progress</p>
 
-              <p className="text-gray-500">58%</p>
+              <p className="text-gray-500">{progress * 100}%</p>
             </div>
 
             <ProgressBar
-              completed={progress}
-              bgColor={progress < 50 ? "#FF0000" : "#00FF00"}
+              completed={progress * 100}
+              bgColor={progress * 100 < 50 ? "#FF0000" : "#00FF00"}
               height="16px"
               isLabelVisible={false}
             />
@@ -89,18 +104,37 @@ export default function ProjectContent() {
           <div>
             {/* cms shit */}
             <div className="flex justify-between mt-12 items-center">
-              <h1 className="text-2xl font-bold">Project Description</h1>
+              <h1 className="text-2xl font-bold">{currentMateri.title}</h1>
               <Image src="/tts.svg" width={30} height={50} alt="Logo" />
             </div>
+            {currentMateri?.image?.responsiveImage &&
+              currentMateri?.image?.responsiveImage.webpSrcSet && (
+                <ResponsiveImage
+                  data={currentMateri?.image?.responsiveImage}
+                  className="object-cover w-full h-auto min-h-full"
+                  layout="responsive"
+                />
+              )}
             <p className="text-gray-500 text-lg mt-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              tincidunt, nunc nec aliquet ultricies, nunc eros ultrices nisi,
-              vel tincidunt risus justo sit amet nunc. Nulla facilisi. Sed ac
-              odio placerat, aliquam nunc nec, ultrices nunc. Nullam ut nisl ac
-              lacus ultricies tincidunt. Donec auctor, purus vel ultricies
-              consectetur, tortor libero aliquam justo, ac ultricies urna lacus
-              in nulla.
+              <StructuredText data={currentMateri?.content1} />
             </p>
+
+            {currentMateri?.video && (
+              <VideoPlayer data={currentMateri?.video} />
+            )}
+
+            <p className="text-gray-500 text-lg mt-4">
+              <StructuredText data={currentMateri?.content} />
+            </p>
+
+            {currentMateri?.file?.responsiveImage &&
+              currentMateri?.file?.responsiveImage.webpSrcSet && (
+                <ResponsiveImage
+                  data={currentMateri?.file?.responsiveImage}
+                  className="object-cover w-full h-auto min-h-full"
+                  layout="responsive"
+                />
+              )}
           </div>
 
           <button className="bg-primary py-2 px-4 w-32 text-white rounded-lg">
