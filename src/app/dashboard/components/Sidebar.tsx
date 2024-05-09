91b281a9ace458db0a6694c99bb229abcb402861
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2024-05-09 10:56:26 +0700

    feat: mentor & project dummy video

--- src/app/dashboard/components/Sidebar.tsx
+++ src/app/dashboard/components/Sidebar.tsx
@@ -16,7 +16,14 @@ export default function Sidebar({ active }: SidebarProps) {
     <div className="flex bg-primary min-h-screen w-[100px] md:w-[300px] text-white py-28  flex-col justify-between">
       <div className="flex flex-col gap-16">
         <div className="flex flex-col items-center">
-          <Image src="/logo-epidemit.svg" width={174} height={100} alt="Logo" />
+          <Link href={"/"}>
+            <Image
+              src="/logo-epidemit.svg"
+              width={174}
+              height={100}
+              alt="Logo"
+            />
+          </Link>
           <h1 className="text-md md:text-xl text-center font-semibold">
             Dashboard
           </h1>
@@ -55,10 +62,13 @@ export default function Sidebar({ active }: SidebarProps) {
         </div>
       </div>
 
-      <div className="flex items-center self-center w-4/5 justify-center hover:bg-secondary hover:text-primary cursor-pointer p-2 rounded-full">
+      <Link
+        href={"/"}
+        className="flex items-center self-center w-4/5 justify-center hover:bg-secondary hover:text-primary cursor-pointer p-2 rounded-full"
+      >
         <IoIosArrowRoundBack className="text-4xl" />
         <span className=" text-lg md:text-2xl">Back</span>
-      </div>
+      </Link>
     </div>
   );
 }
--- src/app/mentor/component/MentorBox.tsx
+++ src/app/mentor/component/MentorBox.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { FaStar } from "react-icons/fa";
 import { IoMdChatboxes } from "react-icons/io";
 import { IoBagOutline } from "react-icons/io5";
@@ -18,15 +20,22 @@ export default function MentorBox({
   reviews,
   sessions,
 }: MentorBoxProps) {
+  const router = useRouter();
   return (
-    <div className="rounded-lg p-2 pb-6 bg-[#f5f4f4] flex flex-col justify-center gap-6 shadow-lg">
-      <Image
-        src={imageUrl}
-        width={320}
-        height={100}
-        alt="Mentor Ava"
-        className="rounded-lg object-cover"
-      />
+    <div
+      onClick={() => {
+        router.push("/mentor/1");
+      }}
+      className="cursor-pointer rounded-lg p-2 pb-6 bg-[#f5f4f4] flex flex-col justify-center gap-6 shadow-lg"
+    >
+      <div className="w-80 aspect-square bg-slate-300 relative overflow-hidden">
+        <Image
+          src={imageUrl}
+          fill={true}
+          alt="profile"
+          className="object-cover w-full h-full"
+        />
+      </div>
 
       <h1 className="text-2xl font-medium text-center">{name}</h1>
 
