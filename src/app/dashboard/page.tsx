768e741ee3164e30be8a6a671a14e2c17f0e7ecc
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2024-05-09 11:06:08 +0700

    feat: FinAid

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import Dashboard from "./components/Dashboard";
-import FinancialAid from "./components/FinancialAid";
 
 export default function Page() {
   return (
     <>
       <Dashboard />
-      <FinancialAid />
     </>
   );
 }
