4488b43aad727e47103497e37150d425f6d82fd6
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2024-05-16 19:20:47 +0700

    colorblind

--- src/app/(mentor)/mentor/component/ProjectPage.tsx
+++ src/app/(mentor)/mentor/component/ProjectPage.tsx
@@ -6,6 +6,7 @@ import { CiFilter } from "react-icons/ci";
 import ProjectPromoBox from "../../project/component/ProjectPromoBox";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 interface ProjectProps {
   id: string;
@@ -39,7 +40,14 @@ export default function ProjectPage({ data }: { data: ProjectProps[] }) {
     <>
       <div className="flex flex-col gap-12 justify-start w-full mt-44">
         {/* box for promo */}
-        <div className="w-full flex flex-col gap-8 lg:flex-row lg:items-center justify-between rounded-lg text-white py-6 lg:py-16 px-7 lg:px-14 h-fit lg:h-96 bg-no-repeat bg-cover bg-[url('/proj-bg.svg')]">
+        <div
+          className="w-full flex flex-col gap-8 lg:flex-row lg:items-center justify-between rounded-lg text-white py-6 lg:py-16 px-7 lg:px-14 h-fit lg:h-96 bg-no-repeat bg-cover"
+          style={{
+            backgroundImage: `url(${
+              cblind ? "/proj-bg-purp.svg" : "/proj-bg.svg"
+            })`,
+          }}
+        >
           <div className="flex flex-col gap-8 lg:w-2/5">
             <h1 className="text-4xl lg:text-6xl font-medium">ProjectiT!</h1>
             <p
@@ -76,17 +84,29 @@ export default function ProjectPage({ data }: { data: ProjectProps[] }) {
                 placeholder="Search Projects"
                 className="w-full bg-transparent outline-none"
               />
-              <button className="px-4 py-2 bg-secondary-dark rounded-full text-white font-semibold">
+              <button
+                className={`px-4 py-2 ${
+                  cblind ? "bg-purple-500" : "bg-secondary-dark"
+                } rounded-full text-white font-semibold`}
+              >
                 Search
               </button>
             </div>
 
             <div className="flex gap-4 justify-end">
-              <div className="bg-[#119ABE] flex items-center rounded-lg p-1">
+              <div
+                className={` ${
+                  cblind ? "bg-purple-500" : "bg-[#119ABE]"
+                } flex items-center rounded-lg p-1`}
+              >
                 <CgSortAz className="text-4xl text-white" />
               </div>
 
-              <div className="bg-[#119ABE] flex items-center rounded-lg p-1">
+              <div
+                className={`${
+                  cblind ? "bg-purple-500" : "bg-[#119ABE]"
+                } flex items-center rounded-lg p-1`}
+              >
                 <CiFilter className="text-4xl text-white" />
               </div>
             </div>
