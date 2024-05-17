ec9ce53a382cbbbb3541632f57c59c7e2452bd78
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-05-17 22:03:08 +0700

    hotfix: cors policy

--- src/app/(mentor)/project/component/FinancialAid.tsx
+++ src/app/(mentor)/project/component/FinancialAid.tsx
@@ -43,6 +43,7 @@ const FinancialAid = () => {
         method: "POST",
         headers: {
           Authorization: `Bearer ${token}`,
+          "Access-Control-Allow-Origin": "*",
         },
       }
     );
